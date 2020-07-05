d7df8aabbeea28f1ec555444f12b632cb717ee95
MadhusBucket <flewdemons@gmail.com>
2020-07-05 16:15:05 +0530

    Updates

---
+++ precache-manifest.657a178dae465de406fd482bfd222e44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc66752d57855f957608aef1402c9519",
+    "url": "/Robo-Friends-React-App/index.html"
+  },
+  {
+    "revision": "4dc288734b5b6b039016",
+    "url": "/Robo-Friends-React-App/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "91ac3962ac1bf9aa624d",
+    "url": "/Robo-Friends-React-App/static/css/main.92b99fc6.chunk.css"
+  },
+  {
+    "revision": "4dc288734b5b6b039016",
+    "url": "/Robo-Friends-React-App/static/js/2.5924c4a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robo-Friends-React-App/static/js/2.5924c4a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91ac3962ac1bf9aa624d",
+    "url": "/Robo-Friends-React-App/static/js/main.2bdf3135.chunk.js"
+  },
+  {
+    "revision": "ef9bece3355193259450",
+    "url": "/Robo-Friends-React-App/static/js/runtime-main.26510be1.js"
+  }
+]);
